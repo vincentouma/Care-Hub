b8fe181295a7e46a4294b375466af7fabaf6006d
JOFLIX <joflixooko@outlook.com>
2019-08-14 14:10:18 +0300

    done routing of the views

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Desktop/carehub/virtual"
+set -gx VIRTUAL_ENV "/home/moringa-school/Documents/Care-Hub/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
