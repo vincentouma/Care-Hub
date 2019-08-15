ad4cdb1806fc240915c77896d888053c26da3697
vincentouma <vinceoumah@gmail.com>
2019-08-15 15:04:19 +0300

    update development

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa-school:123@localhost/blog'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:vinceobindi1005@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = 'mannuh'
